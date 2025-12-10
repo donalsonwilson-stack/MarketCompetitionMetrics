5002013ad5edcb5ec6ec47978e80c82894c10df4
Donalson WILSON <donalsonwilson004@gmail.com>
2025-12-10 23:48:34 +0100

    Update test-lerner.R

--- tests/testthat/test-lerner.R
+++ tests/testthat/test-lerner.R
@@ -1,18 +1,22 @@
-# tests/testthat/test-lerner.R
 library(testthat)
 
 test_that("Lerner index matches expected per period", {
   df <- build_df()
-  library(MarketCompetitionMetrics)
-  res <- lerner(df,
-                firm_col = "Firm",
-                price_col = "Price",
-                cost_col = "Marginal_cost",
-                share_col = "Market_share",
-                period_col = "Period",
-                plot = FALSE)
+  res_lerner <- MarketCompetitionMetrics$lerner(
+    df,
+    firm_col = "Firm",
+    price_col = "Price",
+    cost_col = "Marginal_cost",
+    share_col = "Market_share",
+    period_col = "Period",
+    plot = FALSE
+  )
+
   for (p in names(expected_lerner)) {
-    val <- extract_period_value(res, p, key_candidates = c("Lerner","lerner","L","value"))
-    expect_equal(as.numeric(val), expected_lerner[p], tolerance = 2e-3)
+    # key_candidates include likely names used by your function's output
+    val <- extract_period_value(res_lerner, p, key_candidates = c("market","Market","Lerner","lerner"))
+    expect_false(is.na(val), info = paste("Lerner is NA for period", p))
+    expect_equal(as.numeric(val), as.numeric(expected_lerner[p]), tolerance = 1e-6)
   }
 })
+

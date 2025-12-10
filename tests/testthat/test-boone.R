ad0c27d45559f92ddabba62bdbed76047c74e83e
Donalson WILSON <donalsonwilson004@gmail.com>
2025-12-10 23:48:01 +0100

    Update test-boone.R

--- tests/testthat/test-boone.R
+++ tests/testthat/test-boone.R
@@ -1,16 +1,18 @@
-# tests/testthat/test-boone.R
 library(testthat)
 
 test_that("Boone indicator matches expected per period", {
   df <- build_df()
-  library(MarketCompetitionMetrics)
-  res <- boone(df,
-              cost_cols = c("Labor_cost","Capital_cost","Wage_cost"),
-              profit_col = "Profit",
-              period_col = "Period",
-              plot = FALSE)
+  res_boone <- MarketCompetitionMetrics$boone(
+    df,
+    cost_cols = c("Labor_cost","Capital_cost","Wage_cost"),
+    profit_col = "Profit",
+    period_col = "Period",
+    plot = FALSE
+  )
+
   for (p in names(expected_boone)) {
-    val <- extract_period_value(res, p, key_candidates = c("Boone","boone","beta","beta_sum","value"))
-    expect_equal(as.numeric(val), expected_boone[p], tolerance = 3e-3)
+    val <- extract_period_value(res_boone, p, key_candidates = c("Boone","boone","beta","value"))
+    expect_false(is.na(val), info = paste("Boone is NA for period", p))
+    expect_equal(as.numeric(val), as.numeric(expected_boone[p]), tolerance = 1e-6)
   }
 })

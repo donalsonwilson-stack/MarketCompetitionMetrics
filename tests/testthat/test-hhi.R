ac05cffba60fc6823d407e4a5b703ac3e196bfd4
Donalson WILSON <donalsonwilson004@gmail.com>
2025-12-11 00:22:59 +0100

    Update test-hhi.R

--- tests/testthat/test-hhi.R
+++ tests/testthat/test-hhi.R
@@ -2,7 +2,12 @@ library(testthat)
 
 test_that("HHI matches expected per period", {
   df <- build_df()
-  res_hhi <- MarketCompetitionMetrics$hhi(df, share_col = "Market_share", period_col = "Period", plot = FALSE)
+  res_hhi <- MarketCompetitionMetrics$hhi(
+    df,
+    share_col = "Market_share",
+    period_col = "Period",
+    plot = FALSE
+  )
 
   for (p in names(expected_hhi)) {
     val <- extract_period_value(res_hhi, p, key_candidates = c("HHI","hhi"))
